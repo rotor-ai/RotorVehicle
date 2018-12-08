1c8c127a5d97e396879dbe2041b7dbddc4992d43
Robbie <humphreyrp92@gmail.com>
2018-12-08 17:35:59 -0500

    updates

--- app/src/main/java/ai/rotor/rotorvehicle/MainActivity.java
+++ app/src/main/java/ai/rotor/rotorvehicle/MainActivity.java
@@ -2,16 +2,17 @@ package ai.rotor.rotorvehicle;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 import android.util.Log;
+import android.content.Intent;
 
+import com.google.android.things.bluetooth.BluetoothClassFactory;
 import com.google.android.things.bluetooth.BluetoothConfigManager;
 import com.google.android.things.bluetooth.BluetoothConnectionManager;
 import com.google.android.things.bluetooth.BluetoothPairingCallback;
 import com.google.android.things.bluetooth.PairingParams;
-
-import timber.log.Timber;
 
 /**
  * Skeleton of an Android Things activity.
@@ -35,6 +36,8 @@ import timber.log.Timber;
 public class MainActivity extends Activity {
     private BTPairingCallback pairingCallback = new BTPairingCallback();
     BluetoothAdapter btAdapter;
+    static int REQUEST_TURN_BT_ON = 1;
+    static int REQUEST_MAKE_DISCOVERABLE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,19 +50,54 @@ public class MainActivity extends Activity {
         BluetoothConfigManager btConfigManager = BluetoothConfigManager.getInstance();
 
         btConfigManager.setIoCapability(BluetoothConfigManager.IO_CAPABILITY_IO);
-        
+        btConfigManager.setBluetoothClass(BluetoothClassFactory.build(BluetoothClass.Service.INFORMATION, BluetoothClass.Device.TOY_VEHICLE));
+
+        Log.d("MainActivity","Checking if Bluetooth is on...");
+        if (!btAdapter.isEnabled()) {
+            Log.d("MainActivity", "Turning Bluetooth on...");
+            turnBTRadioON();
+        } else {
+            Log.d("MainActivity", "Starting discoverability...");
+            startDiscoverability();
+        }
+
+        BluetoothConnectionManager.getInstance().registerPairingCallback(pairingCallback);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("MainActivity", "Resuming...");
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TURN_BT_ON) {
+            startDiscoverability();
+        }
+        else if (requestCode == REQUEST_MAKE_DISCOVERABLE) {
+            Log.d("MainActivity", "Starting BTListenForConnectionIS");
+            startService(BTListenForConnectionIS.makeIntent(this));
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        BluetoothConnectionManager.getInstance().unregisterPairingCallback(pairingCallback);
+        super.onDestroy();
     }
 
     private class BTPairingCallback implements BluetoothPairingCallback {
         @Override
         public void onPairingInitiated(BluetoothDevice bluetoothDevice, PairingParams pairingParams) {
             BluetoothConnectionManager.getInstance().finishPairing(bluetoothDevice);
+            Log.d("MainActivity", "Pairing Initiated...");
         }
+    }
+
+    private void turnBTRadioON() {
+        startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), REQUEST_TURN_BT_ON);
+    }
+
+    private void startDiscoverability() {
+        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        intent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 60);
+        startActivityForResult(intent, REQUEST_MAKE_DISCOVERABLE);
     }
 }
---
+++ app/src/main/java/ai/rotor/rotorvehicle/RotorUtils.java
@@ -0,0 +1,8 @@
+package ai.rotor.rotorvehicle;
+
+import java.util.UUID;
+
+public class RotorUtils {
+    static String VEHICLE_NAME_REGEX = "RTR.\\d";
+    static UUID ROTOR_UUID = UUID.fromString("4204ff84-190d-4cce-9e98-526915402758");
+}
