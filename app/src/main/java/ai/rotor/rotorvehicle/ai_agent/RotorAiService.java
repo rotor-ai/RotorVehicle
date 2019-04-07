2dffb69404ff7c34cb31766c46ace58771c4cc6a
Robert Humphrey <humphreyrp92@gmail.com>
2019-04-06 22:32:27 -0400

    adding opencv dependencies, creating initial ai agent classes

---
+++ app/src/main/java/ai/rotor/rotorvehicle/ai_agent/RotorAiService.java
@@ -0,0 +1,17 @@
+package ai.rotor.rotorvehicle.ai_agent;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+
+public class RotorAiService implements Runnable {
+    private RotorCamera mCamera;
+    private Handler mCameraHandler;
+    private HandlerThread mCameraThread;
+
+
+
+    @Override
+    public void run() {
+
+    }
+}
