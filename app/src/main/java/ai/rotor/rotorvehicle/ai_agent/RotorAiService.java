74c6063e82cc9b952d47b13d66d5738d7c85a130
Robert Humphrey <humphreyrp92@gmail.com>
2019-04-07 20:17:20 -0400

    adding RotorCamera and AiService

--- app/src/main/java/ai/rotor/rotorvehicle/ai_agent/RotorAiService.java
+++ app/src/main/java/ai/rotor/rotorvehicle/ai_agent/RotorAiService.java
@@ -1,13 +1,25 @@
 package ai.rotor.rotorvehicle.ai_agent;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Handler;
 import android.os.HandlerThread;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.nio.ByteBuffer;
 
 import ai.rotor.rotorvehicle.MainActivity;
 import timber.log.Timber;
@@ -36,15 +48,53 @@ public class RotorAiService implements Runnable {
     };
 
     public void RotorAiService(Context context) {
+        Timber.d("Creating RotorAiService");
         this.mMainContext = context;
+
+        if (mMainContext.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            Timber.d("No permission");
+        }
+
+        mCameraThread = new HandlerThread("CameraBackground");
+        mCameraThread.start();
+        mCameraHandler = new Handler(mCameraThread.getLooper());
+
+        mCamera = RotorCamera.getInstance();
+        mCamera.initializeCamera(mMainContext, mCameraHandler, mOnImageAvailableListener);
+
     }
+
 
     private Context getMainContext() { return mMainContext; }
 
+
     @Override
     public void run() {
-
+        mCamera.startCapturing();
     }
 
 
+    private ImageReader.OnImageAvailableListener mOnImageAvailableListener = new ImageReader.OnImageAvailableListener() {
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            Image jpgImage = reader.acquireLatestImage();
+
+            ByteBuffer imgBuffer = jpgImage.getPlanes()[0].getBuffer();
+            byte[] bytes = new byte[imgBuffer.remaining()];
+            imgBuffer.get(bytes);
+
+            // Byte array to Mat
+            Mat imgMat = Imgcodecs.imdecode(new MatOfByte(bytes), Imgcodecs.IMREAD_UNCHANGED);
+
+            // Image manipulation
+            Imgproc.cvtColor(imgMat, imgMat, Imgproc.COLOR_RGB2GRAY);
+
+            // Convert to bitmap
+            final Bitmap imgBitmap = Bitmap.createBitmap(imgMat.cols(), imgMat.rows(), Bitmap.Config.RGB_565);
+            Utils.matToBitmap(imgMat, imgBitmap);
+            final Bitmap resizedImgBitmap = imgBitmap.createScaledBitmap(imgBitmap, 150, 150, false);
+
+            jpgImage.close();
+        }
+    };
 }
--- app/src/main/java/ai/rotor/rotorvehicle/ai_agent/RotorCamera.java
+++ app/src/main/java/ai/rotor/rotorvehicle/ai_agent/RotorCamera.java
@@ -1,4 +1,177 @@
 package ai.rotor.rotorvehicle.ai_agent;
 
+import android.content.Context;
+import android.graphics.ImageFormat;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
+import android.media.ImageReader;
+import android.os.Handler;
+
+import java.util.Collections;
+
+import timber.log.Timber;
+
 public class RotorCamera {
+    private static final int IMAGE_WIDTH = 320;
+    private static final int IMAGE_HEIGHT = 240;
+    private static final int MAX_IMAGES = 5;
+    private CameraDevice mCameraDevice;
+    private CameraCaptureSession mCaptureSession;
+
+    private ImageReader mImageReader;
+
+    private boolean mStopCapture;
+
+    private static class InstanceHolder {
+        private static RotorCamera mCamera = new RotorCamera();
+    }
+
+
+    public static RotorCamera getInstance() {
+        return InstanceHolder.mCamera;
+    }
+
+    public void initializeCamera(Context context,
+                                 Handler backgroundHandler,
+                                 ImageReader.OnImageAvailableListener imageAvailableListener) {
+        CameraManager manager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+        String[] camIds = {};
+        try {
+            camIds = manager.getCameraIdList();
+        } catch (CameraAccessException e) {
+            Timber.d("Cam access exception getting IDs");
+        }
+
+        if (camIds.length < 1) {
+            Timber.d("No cameras found");
+            return;
+        }
+
+        String id = camIds[0];
+        Timber.d("Using camera id: " + id);
+
+        // Initialize the image processor
+        mImageReader = ImageReader.newInstance(IMAGE_WIDTH, IMAGE_HEIGHT, ImageFormat.JPEG, MAX_IMAGES);
+        mImageReader.setOnImageAvailableListener(imageAvailableListener, backgroundHandler);
+
+        // Open the camera resource
+        try {
+            manager.openCamera(id, mStateCallback, backgroundHandler);
+        } catch (CameraAccessException e) {
+            Timber.d("Camera access exception");
+            e.printStackTrace();
+        }
+    }
+
+    private final CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {
+            Timber.d("Opened camera");
+            mCameraDevice = camera;
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            Timber.d("Closed camera");
+            mCameraDevice = camera;
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            Timber.d("Error using camera device, closing");
+            camera.close();
+        }
+
+        @Override
+        public void onClosed(CameraDevice camera) {
+            super.onClosed(camera);
+            Timber.d("Closed camera, releasing");
+            mCameraDevice = null;
+        }
+    };
+
+    public void startCapturing() {
+
+        mStopCapture = false;
+
+        if (mCameraDevice == null) {
+            Timber.d("Cannot take picture, camera not initialized");
+            return;
+        }
+
+        try {
+            mCameraDevice.createCaptureSession(
+                    Collections.singletonList(mImageReader.getSurface()),
+                    mSessionCallback,
+                    null);
+        } catch (CameraAccessException e) {
+            Timber.d("access exception while preparing pic");
+            e.printStackTrace();
+        }
+    }
+
+    private CameraCaptureSession.StateCallback mSessionCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(CameraCaptureSession session) {
+            // The camera is already closed
+            if (mCameraDevice == null) {
+                return;
+            }
+
+            // When the session is ready, we start capture
+            mCaptureSession = session;
+            triggerImageCapturing();
+        }
+
+        @Override
+        public void onConfigureFailed(CameraCaptureSession session) {
+            Timber.d("Failed to configure camera");
+        }
+    };
+
+    private void triggerImageCapturing() {
+        try {
+            final CaptureRequest.Builder captureBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+            captureBuilder.addTarget(mImageReader.getSurface());
+            captureBuilder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON);
+            Timber.d("Session initialized");
+            mCaptureSession.setRepeatingRequest(captureBuilder.build(), mCaptureCallback, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private final CameraCaptureSession.CaptureCallback mCaptureCallback = new CameraCaptureSession.CaptureCallback() {
+        @Override
+        public void onCaptureProgressed(CameraCaptureSession session, CaptureRequest request, CaptureResult partialResult) {
+            super.onCaptureProgressed(session, request, partialResult);
+            Timber.d("Partial result");
+        }
+
+        @Override
+        public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
+            super.onCaptureCompleted(session, request, result);
+            if (mStopCapture && mCaptureSession != null) {
+                mCaptureSession.close();
+                mCaptureSession = null;
+
+                Timber.d("Capture session closed");
+            }
+        }
+    };
+
+    public void stopCapturing() {
+        mStopCapture = true;
+    }
+
+    public void shutDown() {
+        if (mCameraDevice != null) {
+            mCameraDevice.close();
+        }
+    }
 }
