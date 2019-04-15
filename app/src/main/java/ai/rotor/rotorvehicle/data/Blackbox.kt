31893e200718535ad1afb4c24affdbc53118eecc
Stuart Bowman <stuartsoft2012@gmail.com>
2019-04-14 20:35:42 -0400

    Minor cleanup in blackbox code

--- app/src/main/java/ai/rotor/rotorvehicle/data/Blackbox.kt
+++ app/src/main/java/ai/rotor/rotorvehicle/data/Blackbox.kt
@@ -2,18 +2,12 @@ package ai.rotor.rotorvehicle.data
 
 import io.reactivex.subjects.BehaviorSubject
 import timber.log.Timber
-import java.sql.Timestamp
-import java.text.DateFormat
 import java.time.Clock
-import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeFormatterBuilder
 import java.time.format.TextStyle
-import java.time.temporal.ChronoField
-import java.time.temporal.TemporalField
-import java.util.*
 import javax.inject.Inject
 
 
@@ -21,36 +15,33 @@ class Blackbox @Inject constructor(val clock: Clock) : Timber.Tree() {
 
     val subject = BehaviorSubject.create<String>()
     private val mahLogs = arrayListOf<String>()
+    private val timestampFormat: DateTimeFormatter = DateTimeFormatterBuilder()
+            .appendPattern("YYYYG-MM-DD HH:mm:ss.SSS ")
+            .appendZoneText(TextStyle.SHORT)
+            .toFormatter()
 
     init {
-        record(getFormatedTimeStamp() + " " + startupMsg)
+        record(getFormattedTimeStamp() + " " + startupMsg)
     }
 
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        record(getFormatedTimeStamp() + " " + message)
+        record(getFormattedTimeStamp() + " " + message)
     }
 
-    fun getLogs(): List<String> {
-        return mahLogs.toList()
-    }
-
+    fun getLogs(): List<String> = mahLogs.toList()
 
     private fun record(line: String) {
         mahLogs.add(line)
         subject.onNext(line)
     }
 
-    private fun getFormatedTimeStamp(): String {
-
-        val timestampFormat = DateTimeFormatterBuilder()
-                .appendPattern("YYYYG-MM-DD HH:mm:ss.SSS ")
-                .appendZoneText(TextStyle.SHORT)
-                .toFormatter()
+    private fun getFormattedTimeStamp(): String {
 
         val timestamp = ZonedDateTime.ofInstant(clock.instant(), ZoneId.of("UTC")).format(timestampFormat)
 
         return "[$timestamp]"
     }
+
     companion object {
         const val startupMsg = "==========BEGINNING OF BLACKBOX LOG=========="
     }
